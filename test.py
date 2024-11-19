1ecc59904a3d36975004467c80a9d170ce2b5a53
lys <lys316@mail.ustc.edu.cn>
2024-11-19 14:53:27 +0800

    20241119

--- test.py
+++ test.py
@@ -13,27 +13,19 @@ def program_prep(origin_blocks):
     for bk in origin_blocks:
         blocks.append([pauliString(ps[0], ps[1]) for ps in bk])
     bn = len(origin_blocks)
-    return blocks # , bn, pn
+    return blocks
 
 def compile():
     with open('./benchmark/H2.pickle', 'rb') as f:
         op_list = pickle.load(f)
-    # print(op_list)
-    # exit()
     blocks = program_prep(op_list)
-    print(blocks)
-    compiler = Compiler(blocks, 'all4')
-    qc = compiler.start('ph')
-    # count_res = {}
-    # count_res['swap'] = qc.count_ops()['swap']
-    # t0 = time()
-    qc = transpile(qc, basis_gates=['cx', 'u3'], optimization_level=3)
-    # print("Qiskit L3:", time()-t0)
-    count_cx = qc.count_ops()['cx']
-    # count_res['depth'] = qc.depth()
-    # print(count_res, '\n')
-    qc.draw('mpl', filename = './H2.png')
-    print(count_cx)
+    compiler = Compiler(blocks, 'grid0505')
+    qc = compiler.start('ucc')
+    print(qc.count_ops())
+    print(qc.qasm())
+    # nSWAP = qc.count_ops()['swap']
+    # depth = qc.depth()
+    # print(nSWAP, ' ', depth)
 
 compile()
 
